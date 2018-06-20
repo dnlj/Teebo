ccbc5c6172e2da3a0e4a9bc7a177b8f6b991c58f
DanielJ <danielwjameson@yahoo.com>
2018-06-19 23:14:33 -0500

    Add point saving to db

--- Teebo/PointsThread.py
+++ Teebo/PointsThread.py
@@ -1,5 +1,7 @@
 import threading
 import time
+import sqlite3
+from contextlib import closing
 
 
 class PointsThread(threading.Thread):
@@ -10,23 +12,52 @@ class PointsThread(threading.Thread):
 		self.pointInterval = pointInterval
 		self.pointAmount = pointAmount
 		self.client = client
+		self.dbfile = "userdata.db"
+		self.databaseInit()
+		
 	
+	def databaseInit(self):
+		with sqlite3.connect(self.dbfile) as con:
+			with closing(con.cursor()) as cur:
+				cur.execute('''
+					CREATE TABLE IF NOT EXISTS users (
+						id INTEGER PRIMARY KEY NOT NULL,
+						username TEXT UNIQUE NOT NULL,
+						points INTEGER NOT NULL DEFAULT 0
+					)
+				''')
+		
 	
 	def updatePoints(self):
 		if (self.pointInterval + self.pointTime) - time.perf_counter() > 0:
 			return
 		
-		self.pointsTime = time.perf_counter()
+		self.pointTime = time.perf_counter()
 		
+		# TODO: Should use a new DB or table for each channel?
 		for chan in self.client.channels:
 			self.updatePointsForChannel(chan)
 	
 	
 	def updatePointsForChannel(self, channel):
 		userList = self.client.getUserListForChannel(channel)
-		if not userList: return
-		# TODO: points
 		
+		if not userList: return
+		
+		with sqlite3.connect(self.dbfile) as con:
+			with closing(con.cursor()) as cur:
+				for user in userList:
+					cur.execute("UPDATE users SET points = points + :amount WHERE username = :user", {
+						"user": user,
+						"amount": self.pointAmount,
+					})
+					
+					if cur.rowcount < 1:
+						cur.execute("INSERT INTO users (username, points) VALUES (:user, :amount)", {
+							"user": user,
+							"amount": self.pointAmount,
+						})
+					
 	
 	def run(self):
 		while True:
